eaefec389ef4868f1bdd54ffe50e7347c93ebf1c
danielfmpires <danielpires@campus.ul.pt>
2019-01-17 15:59:07 +0000

    changed ProfileSc

--- src/main/java/org/fenixedu/accessControl/domain/ProfileSC.java
+++ src/main/java/org/fenixedu/accessControl/domain/ProfileSC.java
@@ -3,52 +3,9 @@ package org.fenixedu.accessControl.domain;
 import org.fenixedu.bennu.core.domain.Bennu;
 import org.fenixedu.bennu.core.domain.User;
 import org.fenixedu.bennu.core.domain.groups.PersistentGroup;
+import org.fenixedu.bennu.core.groups.DynamicGroup;
 
 public class ProfileSC extends ProfileSC_Base {
-
-    @Override
-    public void addMember(User member) {
-        // TODO Auto-generated method stub
-
-        getGroupSet().forEach(group -> {
-
-        });
-
-        super.addMember(member);
-    }
-
-    @Override
-    public void removeMember(User member) {
-        // TODO Auto-generated method stub
-
-        getGroupSet().forEach(group -> {
-
-        });
-
-        super.removeMember(member);
-    }
-
-    @Override
-    public void addGroup(PersistentGroup group) {
-        // TODO Auto-generated method stub
-
-        getMemberSet().forEach(user -> {
-
-        });
-
-        super.addGroup(group);
-    }
-
-    @Override
-    public void removeGroup(PersistentGroup group) {
-        // TODO Auto-generated method stub
-
-        getMemberSet().forEach(user -> {
-
-        });
-
-        super.removeGroup(group);
-    }
 
     public ProfileSC() {
         super();
@@ -62,6 +19,51 @@ public class ProfileSC extends ProfileSC_Base {
         } else {
             throw new Error("Profile " + name + " already exists!");
         }
+    }
+
+    @Override
+    public void addMember(User member) {
+
+        getGroupSet().forEach(group -> {
+            final DynamicGroup dynamic = (DynamicGroup) group.toGroup();
+            dynamic.mutator().changeGroup(dynamic.underlyingGroup().grant(member));
+        });
+
+        super.addMember(member);
+    }
+
+    @Override
+    public void removeMember(User member) {
+
+        getGroupSet().forEach(group -> {
+            final DynamicGroup dynamic = (DynamicGroup) group.toGroup();
+            dynamic.mutator().changeGroup(dynamic.underlyingGroup().revoke(member));
+        });
+
+        super.removeMember(member);
+    }
+
+    @Override
+    public void addGroup(PersistentGroup group) {
+
+        final DynamicGroup dynamic = (DynamicGroup) group.toGroup();
+
+        getMemberSet().forEach(user -> {
+            dynamic.mutator().changeGroup(dynamic.underlyingGroup().grant(user));
+        });
+
+        super.addGroup(group);
+    }
+
+    @Override
+    public void removeGroup(PersistentGroup group) {
+        final DynamicGroup dynamic = (DynamicGroup) group.toGroup();
+
+        getMemberSet().forEach(user -> {
+            dynamic.mutator().changeGroup(dynamic.underlyingGroup().revoke(user));
+        });
+
+        super.removeGroup(group);
     }
 
     public static ProfileSC findByName(String name) {
