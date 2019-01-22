94aea77bb14987f41d190f4aa368ef2131ecfd30
danielfmpires <danielpires@campus.ul.pt>
2019-01-22 16:14:10 +0000

    added menus screen

---
+++ src/main/java/org/fenixedu/accessControl/ui/menus/MenusController.java
@@ -0,0 +1,47 @@
+package org.fenixedu.accessControl.ui.menus;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.fenixedu.bennu.portal.domain.MenuContainer;
+import org.fenixedu.bennu.portal.domain.MenuItem;
+import org.fenixedu.bennu.portal.domain.PortalConfiguration;
+import org.fenixedu.bennu.spring.portal.SpringApplication;
+import org.fenixedu.bennu.spring.portal.SpringFunctionality;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@RequestMapping("/items")
+@SpringApplication(group = "academic(MANAGE_AUTHORIZATIONS)", path = "menus", title = "title.Accesscontrol.Menus",
+        hint = "Access Control")
+@SpringFunctionality(app = MenusController.class, title = "title.Accesscontrol.Menus")
+public class MenusController {
+
+    @RequestMapping(method = RequestMethod.GET)
+    private String init(Model model) {
+
+        final Set<MenuItem> functionalities = getMenu(PortalConfiguration.getInstance().getMenu().getOrderedChild());
+
+        model.addAttribute("menus", functionalities);
+        return "menus/menus";
+    }
+
+    private Set<MenuItem> getMenu(Set<MenuItem> menus) {
+
+        final Set<MenuItem> items = new HashSet<>();
+
+        for (final MenuItem menuItem : menus) {
+            if (menuItem.isMenuContainer()) {
+                final Set<MenuItem> submenus = ((MenuContainer) menuItem).getOrderedChild();
+                items.addAll(getMenu(submenus));
+            } else {
+                items.add(menuItem);
+            }
+        }
+
+        return items;
+
+    }
+
+}
